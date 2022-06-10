1733be21e159098f22f4b86890d4f49d8221cdbb
Oluwabamigbe <69978058+Oluwabamigbe@users.noreply.github.com>
2022-06-10 21:03:48 +0100

    Add files via upload

---
+++ gram.py
@@ -0,0 +1,14 @@
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    x = []
+    for i in word:
+        y = i in anagram
+        x.append(y)
+
+    if (False in x):
+        return False
+    else:
+        return True
+        
+print(find_anagram('kk', 'jh'))
+
